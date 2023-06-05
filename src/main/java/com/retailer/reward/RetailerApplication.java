c4eb66980f505d4abc363b8dd1c6748f1e88aee8
sendmailtoprakash <sendmailtoprakash@gmail.com>
2023-06-05 00:58:36 -0400

    Add files via upload

--- src/main/java/com/retailer/reward/RetailerApplication.java
+++ src/main/java/com/retailer/reward/RetailerApplication.java
@@ -1,1 +1,18 @@
+package com.retailer.reward;
 
+import com.retailer.reward.dao.RetailerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.retailer")
+@SpringBootApplication
+public class RetailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailerApplication.class, args);
+    }
+
+}
